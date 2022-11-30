06647dfc167af2415b95f53a5d7f77ef864cfc2e
mateusriff <mateusnriff@gmail.com>
2022-11-30 11:03:21 -0300

    added function declarations

--- bit_parity.c
+++ bit_parity.c
@@ -1,6 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int verify_argc_input(int argc); 
+int verify_number_input(int number);
+int count_1_bits(int number);
+void print_binary(int number);
+
+int main(int argc, char *argv[]) {
+    
+    int number;
+    int bits = 0;
+    
+    if (verify_argc_input(argc) == 1) return 1;
+    
+    number = atoi(argv[1]);
+    if (verify_number_input(number) == 1) return 1;
+
+    printf("The informed number was: %d -> ", number);
+    print_binary(number); 
+    bits = count_1_bits(number); 
+
+    if (bits % 2 == 0) { 
+        printf("The number is already even\n");
+    } else {
+        printf("The new number is: %d -> ", (number + 128));
+        print_binary(number + 128);
+    }
+    return 0;
+}
+
 int verify_argc_input(int argc) {
     if (argc != 2) {
         printf("Invalid number of arguments\n");
@@ -47,27 +75,4 @@ void print_binary(int number) {
         number = number << 1;
     }
     printf("\n");
-}
-
-int main(int argc, char *argv[]) {
-    
-    int number;
-    int bits = 0;
-    
-    if (verify_argc_input(argc) == 1) return 1;
-    
-    number = atoi(argv[1]);
-    if (verify_number_input(number) == 1) return 1;
-
-    printf("The informed number was: %d -> ", number);
-    print_binary(number); 
-    bits = count_1_bits(number); 
-
-    if (bits % 2 == 0) { 
-        printf("The number is already even\n");
-    } else {
-        printf("The new number is: %d -> ", (number + 128));
-        print_binary(number + 128);
-    }
-    return 0;
 }
